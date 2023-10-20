1023c87cfd6c73c749ab69705c40ff65557bafc5
Haritha1914 <harithachandika@gmail.com>
2023-10-20 10:49:01 +0530

    Controller Changed

--- src/main/java/com/cg/controller/CarController.java
+++ src/main/java/com/cg/controller/CarController.java
@@ -2,12 +2,12 @@ package com.cg.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.cg.entity.Car;
 import com.cg.service.CarServiceImpl;
 
@@ -15,22 +15,19 @@ import com.cg.service.CarServiceImpl;
  
 
 @RestController
+@RequestMapping("/carapi")
 public class CarController {
-	@Autowired
-	CarServiceImpl service;
-	@GetMapping("/car/{regnum}")             
-	private Car getcarByRegnum(@PathVariable("regnum") int regnum) {
-		return service.getcarByRegnum(regnum);
+	
+	private CarServiceImpl cs;
+	
+	@PostMapping("/add")
+	public String addCar(@RequestBody Car car)
+	{
+		return cs.addCar(car);
 	}
-
-	@PostMapping("/car")
-	private int saveOrUpdate(@RequestBody Car car) {
-		service.saveOrUpdate(car);
-		return car.getRegnum();
-	}   
-	@GetMapping("/car")  
-	private List<Car> getAllBooks()   
-	{  
-	return service.getAllCar();  
-	}  
+	
+	@GetMapping("/")
+	public List<Car> getAllCars(){
+		return cs.getCarList();
+	}
 }
--- src/main/java/com/cg/service/CarServiceImpl.java
+++ src/main/java/com/cg/service/CarServiceImpl.java
@@ -1,32 +1,28 @@
 package com.cg.service;
-import java.util.ArrayList;
 import java.util.List;
-
- 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
- 
-
+import com.cg.dao.CarDao;
 import com.cg.entity.Car;
 import com.cg.repository.CarRepository;
 
 @Service
-public class CarServiceImpl {
+public class CarServiceImpl implements CarDao{
+
 	@Autowired
-	CarRepository carRepository;
-	public void saveOrUpdate(Car car) {
-		carRepository.save(car);
+	private CarRepository cr;
+	
+	public String addCar(Car car) {
+		cr.save(car);
+		
+		return "Car Added";
 	}
 
-	public Car getcarByRegnum(int regnum) {
-		return carRepository.findById(regnum).get();
-	} 
-	public List<Car> getAllCar()   
-	{  
-	List<Car> car = new ArrayList<Car>();  
-	carRepository.findAll().forEach(car1 -> car.add(car1));  
-	return car;  
-	}  
+	
+	public List<Car> getCarList() {
+		return cr.findAll();
+	}
+	
 }
